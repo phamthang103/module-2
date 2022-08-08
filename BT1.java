fe2e803fce6666ddbfd406df09c8ad3d014a0075
phamthang103 <106536840+phamthang103@users.noreply.github.com>
2022-08-08 19:05:58 +0700

    Add files via upload

---
+++ BT1.java
@@ -0,0 +1,12 @@
+package tuan1;
+
+import java.util.Scanner;
+
+public class BT1 {
+    public static void main(String[] args) {
+        System.out.println("enter your name");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println(" your name : " + name);
+    }
+}
